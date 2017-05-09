8bcce2d8620fc2530bb14b3d59cbf0ae62b4c586
Hemant Bhanawat <hemant@snappydata.io>
2017-05-09 14:59:16 +0530

    Refactoring

--- RealTimeOlap/src/main/scala/io/snappydata/cassandra/bulkread/CassandraSparkOps.scala
+++ RealTimeOlap/src/main/scala/io/snappydata/cassandra/bulkread/CassandraSparkOps.scala
@@ -1,7 +1,7 @@
 package io.snappydata.cassandra.bulkread
 
 import com.datastax.spark.connector.cql.CassandraConnector
-import org.apache.spark.sql.{SnappySession, SparkSession}
+import org.apache.spark.sql._
 
 
 /**
@@ -10,9 +10,9 @@ import org.apache.spark.sql.{SnappySession, SparkSession}
 object CassandraSparkOps {
   def main(args: Array[String]) {
 
-    if (args.length != 5){
+    if (args.length != 5) {
       println("CassandraSparkOps cassandrahost snappydataclusterurl keyspace tableName partitionnByColumn")
-      println ("CassandraSparkOps localhost localhost:1527 mykeyspace mytable colName")
+      println("CassandraSparkOps localhost localhost:1527 mykeyspace mytable colName")
       return
     }
     val spark: SparkSession = SparkSession
@@ -24,28 +24,26 @@ object CassandraSparkOps {
       // sys-disk-dir attribute specifies the directory where persistent data is saved
       .getOrCreate
     val snSession = new SnappySession(spark.sparkContext)
-    val sc = spark.sparkContext
-    import com.datastax.spark.connector._ //Loads implicit functions
+    cassandraOps(snSession, args(2), args(3), args(4))
+  }
+
+  def cassandraOps(snSession: SnappySession, keySpace: String, table: String, partitionByCol: String) {
 
     val df1 = snSession
       .read
       .format("org.apache.spark.sql.cassandra")
-      .options(Map( "table" -> args(3), "keyspace" -> args(2) ))
+      .options(Map("table" -> table, "keyspace" -> keySpace))
       .load()
     df1.show()
     df1.printSchema()
-
-    val snappyTbl =s"snappy_${args(3)}"
+    val snappyTbl = s"snappy_${table}"
     snSession.dropTable(snappyTbl, true);
 
-    snSession.createTable(snappyTbl, "column", df1.schema, Map("PARTITION_BY" -> args(4)), true)
+    snSession.createTable(snappyTbl, "column", df1.schema, Map("PARTITION_BY" -> partitionByCol), true)
     // Thread.sleep(100000)
     df1.write.insertInto(snappyTbl)
     snSession.sql(s"select * from $snappyTbl").show
-
     Thread.sleep(1000000)
-
-
   }
 
 }
